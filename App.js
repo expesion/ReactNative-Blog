0d6350dae92d8b1815ff315895b6702de07bd13d
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-05 12:25:26 +0530

    Skeleton Context api setup

--- App.js
+++ App.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-
+import { BlogProvider } from "./src/context/BlogContex";
 import IndexScreen from "./src/screens/IndexScreen";
 const Stack = createStackNavigator();
 
 function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Blogs" component={IndexScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <BlogProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Blogs" component={IndexScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </BlogProvider>
   );
 }
 
