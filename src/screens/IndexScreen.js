db570bc509d84db9e9b523eb09ba24936b2ae3e6
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-05 11:52:45 +0530

    React Navigation 5 setup

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+function IndexScreen() {
+  return (
+    <View>
+      <Text>Index Screen</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({});
+export default IndexScreen;
