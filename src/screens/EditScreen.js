647949095418e5f4a279145b1459f3a9eb7d49ba
Mohammed Thaha <expesion23exnoiu@gmail.com>
2021-05-06 13:20:54 +0530

    Working on Edit Screen

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { View, Text, TextInput, StyleSheet } from "react-native";
+import BlogContext from "../context/BlogContex";
 function EditScreen({ route }) {
-  console.log(route.params.id);
+  const { state: Blogs } = useContext(BlogContext);
+
+  const [info, setInfo] = useState(() => {
+    const blog = Blogs.find((item) => item.id == route.params.id);
+    return blog;
+  });
   return (
     <View>
-      <Text>Edit</Text>
+      <Text style={styles.label}>Enter Title:</Text>
+      <TextInput
+        style={styles.input}
+        value={info.title}
+        onChangeText={(newValue) =>
+          setInfo((prevValue) => ({
+            title: newValue,
+            content: prevValue.content,
+          }))
+        }
+      />
+      <Text style={styles.label}>Enter Content:</Text>
+      <TextInput
+        style={styles.input}
+        value={info.content}
+        onChangeText={(newValue) =>
+          setInfo((prevValue) => ({
+            title: prevValue.title,
+            content: newValue,
+          }))
+        }
+      />
+      <Text>
+        {info.title}
+        {info.content}
+      </Text>
     </View>
   );
 }
-
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 15,
+    padding: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+});
 export default EditScreen;
